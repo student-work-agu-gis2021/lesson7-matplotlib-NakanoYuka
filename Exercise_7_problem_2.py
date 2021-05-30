0719c8eaa02050c5ac0e03975574635db7b802e9
NakanoYuka <>
2021-05-30 15:59:39 +0000

    Save plot as image

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -69,7 +69,7 @@ outputfp = "temp_line_plot.png"
 
 # Save plot as image
 # YOUR CODE HERE 4
-
+plt.savefig(outputfp)
 import os
 
 #Check that output file exists (also open the file and check that the plot looks ok!)
